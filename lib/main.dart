f2b29facf16da1d056a342b8048bfa98f1724245
lucasgilTelco <94456075+lucasgilTelco@users.noreply.github.com>
2025-03-07 14:59:59 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 3).theme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(onPressed: () {}, child: Text('Click me')),
+        ),
+      ),
+    );
+  }
+}
