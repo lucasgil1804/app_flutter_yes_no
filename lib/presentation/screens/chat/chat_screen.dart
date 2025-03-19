28adc50d9097d4ac27f8375abdf9a0a1b8d4da6e
lucasgilTelco <94456075+lucasgilTelco@users.noreply.github.com>
2025-03-18 21:51:52 -0300

    Comportamiento de caja de texto para chat

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 import '../../widgets/chat/her_message_bubble.dart';
+import '../../widgets/chat/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -40,6 +41,7 @@ class _ChatView extends StatelessWidget {
                           ? HerMessageBubble()
                           : MyMessageBubble();
                     })),
+            MessageFieldBox()
           ],
         ),
       ),
---
+++ lib/presentation/widgets/chat/shared/message_field_box.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: BorderSide(color: Colors.transparent),
+      borderRadius: BorderRadius.circular(40),
+    );
+
+    final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "?"',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            textController.clear();
+          },
+        ));
+
+    return TextFormField(
+      onTapOutside: (event) => focusNode.unfocus(),
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        textController.clear();
+        focusNode
+            .requestFocus(); // mantener el foco en el keyboard cuando enviamos un mensaje
+      },
+    );
+  }
+}
