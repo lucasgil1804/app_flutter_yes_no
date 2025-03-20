a4c3b9fc6fcd75b48de0febb7547d2dbc59d346e
lucasgilTelco <94456075+lucasgilTelco@users.noreply.github.com>
2025-03-20 15:34:00 -0300

    Hacer scroll al final cuando envio un mensaje

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,15 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messagesList = [
     Message(text: "Hola amor", fromWho: FromWho.me),
     Message(text: "Ya regresaste del trabajo?", fromWho: FromWho.me)
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
+
     final newMessage = Message(fromWho: FromWho.me, text: text);
     messagesList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() {
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut);
   }
 }
