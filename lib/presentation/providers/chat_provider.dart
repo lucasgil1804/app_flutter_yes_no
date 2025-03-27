8474d3f0f7fa27a03f91b07dcec5cbea910c6342
lucasgilTelco <94456075+lucasgilTelco@users.noreply.github.com>
2025-03-27 15:33:02 -0300

    Entidad para formatear consulta a API yes/no

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messagesList = [
     Message(text: "Hola amor", fromWho: FromWho.me),
-    Message(text: "Ya regresaste del trabajo?", fromWho: FromWho.me)
+    // Message(text: "Ya regresaste del trabajo?", fromWho: FromWho.me)
   ];
 
   Future<void> sendMessage(String text) async {
@@ -15,6 +17,18 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(fromWho: FromWho.me, text: text);
     messagesList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      herReply();
+    }
+
+    notifyListeners();
+    moveScrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+
+    messagesList.add(herMessage);
     notifyListeners();
     moveScrollToBottom();
   }
